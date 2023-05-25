38aa743515e324570c7493b35da19125ead0ea87
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-05-25 13:26:15 -0300

    simple updates in header.jsx and header.css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,10 +10,9 @@ function Header(){
             <img className="logo" src={THT} alt="Logo da THT" />
             <nav>
                 <ul>
-                    <li><a className="instagram" href="#">Instagram</a></li>
-                    <li><a className="linkedin" href="#">Linkedin</a></li>
-                    <li><a className="github" href="#">Github</a></li>
-                    <li><a className="linktree" href="#">Linktree</a></li>
+                    <li><a className="linkedin" href="https://www.linkedin.com/in/igor-perdigao-silva/" target="_blank">Linkedin</a></li>
+                    <li><a className="github" href="https://github.com/Hashtag-igor" target="_blank">Github</a></li>
+                    <li><a className="linktree" href="https://linktr.ee/Hashtag_igor" target="_blank">Linktree</a></li>
                 </ul>
             </nav>
         </header>
